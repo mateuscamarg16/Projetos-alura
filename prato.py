3d70ba4e731772e33d4f48f572ced5f811ff4e5e
mateuscamarg16 <134179657+mateuscamarg16@users.noreply.github.com>
2025-06-10 16:30:46 -0300

    Create prato.py
    class que herdará instancias da classe principal

---
+++ prato.py
@@ -0,0 +1,12 @@
+from modelos.cardápio.item_cardapio import ItemCardapio
+
+
+class Prato(ItemCardapio):
+    def __init__(self, nome, preco, descricao):
+        super().__init__(nome, preco)
+        self._descricao = descricao
+        
+    
+    def __str__(self):
+        return self._nome
+    
